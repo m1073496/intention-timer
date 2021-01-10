72f4325816477ab84b9779b7bfd5bba1b8041fc1
Katie Ballentine <knees4bees@gmail.com>
2021-01-10 13:05:10 -0700

    Remove comments

--- Activity.js
+++ Activity.js
@@ -9,29 +9,16 @@ class Activity {
   }
 
   startTimer() {
-    // useful constants to express normal time units as milliseconds
     var second = 1000;
     var minute = second * 60;
-
-    // new Date() returns current time as a date string
-    // Date.parse(foo) forces foo to be expressed as a number of milliseconds
-    // we *need* timeAtButtonPress to be in milliseconds
-    //   because timerLength below will be in milliseconds and we want to add them together (add apples to apples)
-    var timeAtButtonPress = Date.parse(new Date()); // example: 12:30
-
-    // calculate # of milliseconds our user wants
-    var timerLength = (this.minutes * minute) + (this.seconds * second); // example: 5:00
-
-    // could use Date.parse() here to force targetTime to be in milliseconds, but not required
-    //  (see rightNow below, which will be expressed the same way as our targetTime here,
-    //    so when we do the calculation to calculate timeLeftOnClock, we'll be subtracting oranges from oranges)
-    var calculatedTargetTime = new Date(timeAtButtonPress + timerLength); // example: 12:35
-
+    var timeAtButtonPress = Date.parse(new Date());
+    var timerLength = (this.minutes * minute) + (this.seconds * second);
+    var calculatedTargetTime = new Date(timeAtButtonPress + timerLength);
     var timer;
 
     function determineRemaining() {
-      var rightNow = new Date(); //example: 12:32
-      var timeLeftOnClock = calculatedTargetTime - rightNow; // if function is run at 12:32, then 12:32 minus 12:30 equals 3:00 remaining
+      var rightNow = new Date();
+      var timeLeftOnClock = calculatedTargetTime - rightNow;
       if (timeLeftOnClock < 0) {
         //KATIE PLEASE MOVE TO MAIN.JS FUNCTION FROM HERE TO....
         startTimerButton.innerText = "COMPLETE";
@@ -46,23 +33,12 @@ class Activity {
         return;
       }
 
-      var minutes = Math.floor(timeLeftOnClock / minute); // example: timeLeftOnClock is 3:45 (shitload of milliseconds); minutes left will be 3.75; 'minutes' variable will be 3
-      var seconds = Math.floor((timeLeftOnClock % minute) / second); // example: timeLeftOnClock is 3:45; seconds will be 45
-      // console.log(minutes);
-      // console.log(seconds);
+      var minutes = Math.floor(timeLeftOnClock / minute);
+      var seconds = Math.floor((timeLeftOnClock % minute) / second);
 
       return {
         minutes, seconds
       }
-
-      //   var days = Math.floor(distance / day);
-      //   var hours = Math.floor((distance % day) / hour);
-      //   var minutes = Math.floor((distance % hour) / minute);
-      //   var seconds = Math.floor((distance % minute) / second);
-      //
-      //   return {
-      //     days, hours, minutes, seconds
-      //   }
     }
 
     // TODO: move this DOM manipulation to the main.js file
